cea0c1fb6190b3fcb2fba810353c67199f515221
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-03-29 02:19:03 +0500

    Выполнил второе задание

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
-    alert("Hello, my friends!");
-    console.log("Hello!");
+let money = 250,
+    Income = "music",
+    addExpenses = "интернет, такси, коммуналка",
+    deposit = true,
+    mission = 500000,
+    period = 11;
+console.log(typeof money);
+console.log(typeof Income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+let NewArr = addExpenses.toUpperCase().split(",");
+console.log(NewArr);
+let budgetDay = money/30;
+console.log(budgetDay);
+
+
