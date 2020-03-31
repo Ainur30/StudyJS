75b529135c740bcd38d90a7adfe589e303db76bb
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-03-31 23:53:21 +0500

    Усложненное задание 4-го урока

--- js/file1.js
+++ js/file1.js
@@ -1,45 +1,15 @@
 "use strict";
-// 1)a
-let lang = "en",
-    ruWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
-    enWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-if (lang === "ru") {
-    for (let wDay of ruWeek) {
-        console.log(wDay);
+function checkStr(str) {
+    if (!(typeof(str) === "string")) {
+        str = "Не строка!";
+    } else if (str.trim().length > 30) {
+        str = str.trim().slice(0, 30) + "...";
+    } else {
+        str = str.trim();
     }
+    return str;
+ }
+ 
+alert(checkStr("str"));
+  
 
-} else {
-    for (let wDay of enWeek ) {
-        console.log(wDay);
-    }
-}
-
-// 1)b
-switch (lang) {
-    case "en":
-        for (let wDay of enWeek ) {
-            console.log(wDay);
-        }
-        break;
-    case "ru":
-        for (let wDay of ruWeek) {
-            console.log(wDay);
-        }
-    break;
-}
-
-
-// 1)c
- let week = {
-    "ru": ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
-    "en": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    
-};
-console.log(week[lang][0]);
-
-// 2 задание 
-let namePerson = prompt("Введите свое имя?",'');
-let guest = (namePerson === "Артем") ? "Директор" : 
- (namePerson === "Максим") ? "Преподаватель" : "Студент";
- console.log(guest);
