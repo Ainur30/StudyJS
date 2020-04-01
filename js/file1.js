8fff08bfe98bc62fc7e8aaf8d37b686046d7c2c4
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-04-01 21:52:57 +0500

    Выполнил усложненное задание 5-го урока

--- js/file1.js
+++ js/file1.js
@@ -1,15 +1,22 @@
 "use strict";
-function checkStr(str) {
-    if (!(typeof(str) === "string")) {
-        str = "Не строка!";
-    } else if (str.trim().length > 30) {
-        str = str.trim().slice(0, 30) + "...";
-    } else {
-        str = str.trim();
+//1 задание
+let arr = ["244", "344", "544", "254", "467", "789", "241"];
+function filterRange(arr) {
+    return arr.filter(item => (item[0] === "2" || item[0] === "4"));
     }
-    return str;
- }
- 
-alert(checkStr("str"));
-  
+ let filtered = filterRange(arr);
+ console.log(filtered);
 
+
+//2 задание
+StartAgain:
+for (let i = 2; i <= 100; i++) { 
+
+  for (let j = 2; j < i; j++) { 
+    if (i % j === 0) {
+        continue StartAgain;
+    }
+  }
+
+  console.log(`Делители числа ${i}: 1 и ${i}`); 
+}
