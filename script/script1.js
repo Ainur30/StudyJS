37af8cdd71a060c491d4884de7a04a8f66c233d5
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-04-03 02:57:27 +0500

    Исправил сложное задание5

--- script/script1.js
+++ script/script1.js
@@ -1,74 +1,76 @@
 "use strict";
-function Numb(){
-    function randNumber(){
-        let rand = 1 + Math.random() * 100;
-        return Math.floor(rand);
-    }
-    return randNumber();
-}
- let res = Numb();
-    
 
-function checkNumber(count){
-   
-    
-    let guessNumber = prompt("Угадай число от 1 до 100"),
-        n = 3,
+function game(){
+    let res = Math.floor(1 + Math.random() * 100),
+        count = 10;
+        
+
+    return function checkNumber(){
+        let guessNumber = prompt("Угадай число от 1 до 100"),
+        //count = 10,
         replay;
+       
    
-    if (isNaN(guessNumber) || guessNumber === ""){
-        guessNumber = alert("Введи число!");
+        if (isNaN(guessNumber) || guessNumber === ""){
+        alert("Введи число!");
         checkNumber(count);
-    }
+        }
 
-    if (guessNumber === null){
-        alert("Конец игры");
-        return;
-    }
+        if (guessNumber === null){
+        return alert("Конец игры");
+      
+        }
 
-    if (Number(guessNumber) > res ){
+        if (Number(guessNumber) > res ){
         count--;
         alert(`Загаданное число меньше, осталось попыток ${count}`);
         
         if (count === 0) {
             replay = confirm("Попытки закончились, хотите сыграть еще?");
             if (replay === false){
-                alert("Конец игры");
-                return;
+             return  alert("Конец игры");
+               
              } else {
-                res = Numb();
-                checkNumber(10);
+                let startAgain = game();
+            startAgain();
+                //checkNumber(10);
             }
         } else {checkNumber(count);}
         
 
-    } else if (Number(guessNumber) < res ){
+        }   else if (Number(guessNumber) < res ){
         count--;
         alert(`Загаданное число больше, осталось попыток ${count}`);
         
         if (count === 0) {
             replay = confirm("Попытки закончились, хотите сыграть еще?");
             if (replay === false){
-            alert("Конец игры");
-            return ;
+           
+                return alert("Конец игры"); 
              } else {
-            res = Numb();
-            checkNumber(10);
+                let startAgain = game();
+                startAgain();
+            //checkNumber(10);
             }
             } else {checkNumber(count);}
         
-    } else if (Number(guessNumber) === res){
+        } else if (Number(guessNumber) === res){
 
         replay = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
         if (replay === false){
-            alert("Конец игры");
+            return alert("Конец игры");
         } else {
-            res = Numb();
-            checkNumber(10);
+            let startAgain = game();
+            startAgain();
+            //checkNumber(10);
         }
-    } 
+        }    
 
     
-  
+
+};
+
 }
-checkNumber(10);
+ let startGame = game();
+startGame();
+console.dir(startGame);
