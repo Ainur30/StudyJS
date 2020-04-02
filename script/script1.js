17bd4a9d8c109619580aef8d7156221ed24c4347
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-04-02 21:48:57 +0500

    Сложное задание 6-го урока

---
+++ script/script1.js
@@ -0,0 +1,68 @@
+"use strict";
+
+
+function checkNumber(count){
+    
+    let guessNumber = prompt("Угадай число от 1 до 100"),
+        n = 3,
+        replay;
+
+
+    function randNumber(){
+        let rand = 1 + Math.random() * 100;
+        return Math.floor(rand);
+    }
+   
+    if (isNaN(guessNumber) || guessNumber === ""){
+        guessNumber = alert("Введи число!");
+        checkNumber(count);
+    }
+
+    if (guessNumber === null){
+        alert("Конец игры");
+        return;
+    }
+
+    if (Number(guessNumber) > randNumber() ){
+        count--;
+        alert(`Загаданное число меньше, осталось попыток ${count}`);
+        
+        if (count === 0) {
+            replay = confirm("Попытки закончились, хотите сыграть еще?");
+            if (replay === false){
+                alert("Конец игры");
+                return;
+             } else {
+                checkNumber(10);
+            }
+        } else {checkNumber(count);}
+        
+
+    } else if (Number(guessNumber) < randNumber()){
+        count--;
+        alert(`Загаданное число больше, осталось попыток ${count}`);
+        
+        if (count === 0) {
+            replay = confirm("Попытки закончились, хотите сыграть еще?");
+            if (replay === false){
+            alert("Конец игры");
+            return ;
+             } else {
+            checkNumber(10);
+            }
+            } else {checkNumber(count);}
+        
+    } else if (Number(guessNumber) === randNumber()){
+
+        replay = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+        if (replay === false){
+            alert("Конец игры");
+        } else {
+            checkNumber(10);
+        }
+    } 
+
+    
+  
+}
+checkNumber(10);
