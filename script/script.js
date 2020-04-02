f98e2fdb9ff538935c8b54b1b29116a0255f05b9
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-04-02 21:25:47 +0500

    Задание 6 урока

---
+++ script/script.js
@@ -0,0 +1,31 @@
+"use strict";
+function checkNumber() {
+    let guessNumber = prompt("Угадай число от 1 до 100");
+   
+
+    function randNumber(){
+        let rand = 1 + Math.random() * 100;
+        return Math.floor(rand);
+    }
+    if (isNaN(guessNumber) || guessNumber.trim() === ""){
+        guessNumber = alert("Введи число!");
+        checkNumber();
+
+    }
+
+    if (guessNumber === null || guessNumber === randNumber()) {
+        return;
+    }
+
+    if (Number(guessNumber) > randNumber()) {
+        guessNumber = alert('Загаданное число меньше');
+        checkNumber();
+        
+    } else if (Number(guessNumber) < randNumber()) {
+        guessNumber = alert('Загаданное число больше');
+        checkNumber();
+        
+    }
+   
+}
+checkNumber();
