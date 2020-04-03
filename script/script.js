0352926390eb578b7651dd5dc4f0a972d9161e6e
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-04-03 11:28:59 +0500

    Исправил задание 6

--- script/script.js
+++ script/script.js
@@ -1,32 +1,34 @@
 "use strict";
-function checkNumber() {
-    let guessNumber = prompt("Угадай число от 1 до 100");
-   
+function game() {
+    let rand = Math.floor(1 + Math.random() * 100);
 
-    function randNumber(){
-        let rand = 1 + Math.random() * 100;
-        return Math.floor(rand);
-    }
-    
-    if (isNaN(guessNumber) || guessNumber.trim() === ""){
-        guessNumber = alert("Введи число!");
-        checkNumber();
+    return function checkNumber(){
+        let guessNumber = prompt("Угадай число от 1 до 100");
+      
 
-    }
+        if (isNaN(guessNumber) || guessNumber === ""){
+            alert("Введи число!");
+            checkNumber();
 
-    if (guessNumber === null || guessNumber === randNumber()) {
+        }
+
+        if (guessNumber === null || guessNumber === rand) {
         return;
-    }
+        }
 
-    if (Number(guessNumber) > randNumber()) {
-        guessNumber = alert('Загаданное число меньше');
-        checkNumber();
+        if (Number(guessNumber) > rand) {
+            alert('Загаданное число меньше');
+            checkNumber();
         
-    } else if (Number(guessNumber) < randNumber()) {
-        guessNumber = alert('Загаданное число больше');
-        checkNumber();
+        } else if (Number(guessNumber) < rand) {
+            alert('Загаданное число больше');
+            checkNumber();
         
     }
    
+};
 }
-checkNumber();
+
+let startGame = game();
+startGame();
+console.dir(startGame);
