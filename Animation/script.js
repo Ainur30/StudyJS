ec120f8d066c43cdeaa83ec5c47b0a56843c0f96
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-04-23 00:19:33 +0500

    add fixed lesson18c

--- Animation/script.js
+++ Animation/script.js
@@ -1,60 +1,63 @@
-window.addEventListener('DOMContentLoaded', () => {
 
-    'use strict';
-    const body = document.querySelector('body');
-    const one = document.querySelector('.one');
-    const button = document.querySelector('.button');
-    const reset = document.querySelector('.reset');
-    let k = 0;
-    let n = 0;
-    let m = 0;
-    let move;
-    const animate = () => {
-        move = requestAnimationFrame(animate);
-        k++;
-        if (k < 300) {
-            m++;
-            one.style.top = m + 'px';
-        } else if (k < 600) {
-            body.setAttribute('style', 'background-color: black');
-            n++;
-            one.style.right = n + 'px';
-        } else if (k < 900) {
-            body.setAttribute('style', 'background-color: purple');
-            m--;
-            one.style.top = m + 'px';
-        } else if (k < 1200) {
-            body.setAttribute('style', 'background-color: blue');
-            n--;
-            one.style.right = n + 'px';
-
-        } else {
-            cancelAnimationFrame(move);
-        }
-    };
-    let start = false;
-    button.addEventListener('click', function () {
-        if (start) {
-            start = false;
-            button.textContent = 'Пауза';
-            move = requestAnimationFrame(animate);
-        } else {
-            start = true;
-            button.textContent = 'Запуск';
-            cancelAnimationFrame(move);
-        }
-    });
-    reset.addEventListener('click', () => {
-        one.style.top = 0;
-        one.style.right = 0;
-        start = false;
+'use strict';
+const body = document.querySelector('body');
+const one = document.querySelector('.one');
+const button = document.querySelector('.button');
+const reset = document.querySelector('.reset');
+let k = 0;
+let n = 0;
+let m = 0;
+let move;
+const animate = function () {
+    move = requestAnimationFrame(animate);
+    k++;
+    if (k < 300) {
+        m++;
+        one.style.top = m + 'px';
+    } else if (k < 600) {
+        body.setAttribute('style', 'background-color: black');
+        n++;
+        one.style.right = n + 'px';
+    } else if (k < 900) {
+        body.setAttribute('style', 'background-color: purple');
+        m--;
+        one.style.top = m + 'px';
+    } else if (k < 1200) {
         body.setAttribute('style', 'background-color: blue');
-        button.textContent = 'Запуск';
-        k = 0;
-        n = 0;
-        m = 0;
+        n--;
+        one.style.right = n + 'px';
+    } else {
         cancelAnimationFrame(move);
-    });
+    }
+};
+
+let start = false;
+button.addEventListener('click', function () {
 
 
-}); 
+    if (start) {
+        button.textContent = 'Пауза';
+        cancelAnimationFrame(move);
+        start = false;
+    } else {
+
+        start = true;
+        button.textContent = 'Запуск';
+        move = requestAnimationFrame(animate);
+    }
+
+});
+reset.addEventListener('click', () => {
+    one.style.top = 0;
+    one.style.right = 0;
+    start = false;
+    body.setAttribute('style', 'background-color: blue');
+    button.textContent = 'Запуск';
+    k = 0;
+    n = 0;
+    m = 0;
+    cancelAnimationFrame(move);
+});
+
+
+
