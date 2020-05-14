a9a58437110a080b9897f289ed6cd3550c0ab679
Ainur Shakirov <aynur.shakirov.00.00@mail.ru>
2020-05-14 16:35:21 +0500

    add final project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import dropDownMenu from './modules/dropDownMenu';
+import freeVisitForm from './modules/freeVisitForm';
+import callbackForm from './modules/callbackForm';
+
+dropDownMenu();
+freeVisitForm();
+callbackForm();
